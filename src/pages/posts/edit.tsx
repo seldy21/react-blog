fb56755393d35230998075056bda841243e84720
seldy21 <seldy@naver.com>
2025-04-16 22:34:14 +0900

    route 생성, home css 적용

---
+++ src/pages/posts/edit.tsx
@@ -0,0 +1,3 @@
+export default function PostEdit() {
+  return <h1>Post edit Page</h1>
+}
---
+++ src/pages/posts/indes.tsx
@@ -0,0 +1,8 @@
+export default function PostList() {
+  return (
+    <div>
+      <h1>Post List</h1>
+      <p>Welcome to the post list page!</p>
+    </div>
+  );
+}
